eca3c6bf8051811f89bd95897c8bd4be921488e1
Sekori1 <romaric55220@gmail.com>
2020-04-06 16:19:53 +0200

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :tools
   resources :metiers
   resources :associations
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
