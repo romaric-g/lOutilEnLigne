9206288b0c70c49d9e9d1bf574fa090ce23da174
Sekori1 <romaric55220@gmail.com>
2020-04-08 13:02:07 +0200

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: ENV['S3_KEY'], password: ENV['S3_SECRET']
     before_action :configure_permitted_parameters, if: :devise_controller?
     add_breadcrumb "Accueil", :root_path
   
     protected
-  
+
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
         devise_parameter_sanitizer.permit(:sign_in) do |user_params|
